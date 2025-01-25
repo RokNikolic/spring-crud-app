52444cc601a8155be0482cfc63839a7a8c9c2bbf
Rok Nikolič <rok.nikolic@gmail.com>
2025-01-25 12:31:04 +0100

    api mostly done with no real db

--- src/main/java/com/github/roknikolic/springcrudapp/ProductController.java
+++ src/main/java/com/github/roknikolic/springcrudapp/ProductController.java
@@ -1,12 +1,58 @@
 package com.github.roknikolic.springcrudapp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.*;
 
 @RestController
 public class ProductController {
+    private Map<String, Product> db = new HashMap<>() {{
+        put("1", new Product("1", "name1", "blabla", 0.0F));
+    }};
+
     @GetMapping("/")
-    public String helloWorld() {
-        return "Hello World";
+    public String welcome() {
+        return "Welcome to Product api";
+    }
+
+    @GetMapping("/products")
+    public Collection<Product> read() {
+        return db.values();
+    }
+
+    @GetMapping("/product/{id}")
+    public Product read(@PathVariable String id) {
+        Product product = db.get(id);
+        if (product == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } else {
+            return product;
+        }
+    }
+
+    @DeleteMapping("/product/{id}")
+    public void delete(@PathVariable String id) {
+        Product product = db.remove(id);
+        if (product == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/product")
+    public Product create(@RequestBody @Valid Product product) {
+        System.out.println(product);
+        product.setId(UUID.randomUUID().toString());
+        db.put(product.getId(), product);
+        return product;
+    }
+
+    @PutMapping("/product/{id}")
+    public Product update(@PathVariable String id, @RequestBody Product product) {
+        System.out.println("Updating");
+        //TODO
+        return product;
     }
 }
